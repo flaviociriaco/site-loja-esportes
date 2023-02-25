c680403353b6d82873dff149ce6caaeb0b44d612
Flavio Ciriaco de Moura <flaviociriaco@yahoo.com>
2023-02-25 20:08:24 -0300

    criando arquivo de formulário de login

--- README.md
+++ README.md
@@ -1,18 +1,2 @@
 # site-loja-esportes
 
-Tarefas:
-
--(X)Colocar acima do menu:
--(X)Logo
--(X)Campo de busca de produtos na página
--(X)Campo de entrar e cadastrar
-
--( )Carrinho de compras
--( )Menu:
--( )Menu dropdown
--( )Menu Hamburguer
-
--(X)slide
-
--( )Fotos dos produtos com preço
--( )Rodapé
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+// const raquete = document.querySelector("#raquete");
+// const bola = document.querySelector("#bola");
 
+// let escolha = secao1
+// function buscar() {
+//   switch(escolha) {
+//     case secao1:
+//     return raquete;
+//     break
+//   }
+// }
+// buscar.addEventListener("click", )
+
+function buscar(){
+    document.querySelector('.raquete')
+}
